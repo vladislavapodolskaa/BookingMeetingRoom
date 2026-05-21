c18f277c9ebb42afe8ce030d64dcb436cc450a48
vladislavapodolskaa <vladislavapodolskaa@gmail.com>
2026-05-21 13:29:41 +0300

    Add new validation

--- src/main/java/com/example/bookingmeetingroom/domain/BookingInterval.java
+++ src/main/java/com/example/bookingmeetingroom/domain/BookingInterval.java
@@ -2,29 +2,17 @@ package com.example.bookingmeetingroom.domain;
 
 import java.time.LocalDateTime;
 
-public class BookingInterval {
-    private final LocalDateTime startTime;
-    private final LocalDateTime endTime;
+public record BookingInterval(LocalDateTime startTime, LocalDateTime endTime) {
 
-    public BookingInterval(LocalDateTime startTime, LocalDateTime endTime) {
-        validationInterval(startTime, endTime);
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-    private void validationInterval(LocalDateTime startTime, LocalDateTime endTime){
-        if (endTime.isBefore(startTime)){
+    public BookingInterval {
+        if (endTime.isBefore(startTime)) {
             throw new IllegalArgumentException("End date is before start date");
         }
-        if (endTime.isEqual(startTime)){
+        if (endTime.isEqual(startTime)) {
             throw new IllegalArgumentException("End date is equal start date");
         }
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
+        if (!startTime.toLocalDate().isEqual(endTime.toLocalDate())){
+            throw new IllegalArgumentException("Booking must start and end on the same day");
+        }
     }
 }
